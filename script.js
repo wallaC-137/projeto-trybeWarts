121d1b562ad4eb013c9f5401ea72bfe020918b83
Wallace <wallace_031@hotmail.com>
2022-10-14 16:23:49 -0300

    3. Acrescente um formulário de login no header.

---
+++ script.js
@@ -0,0 +1,19 @@
+const getLogin = document.getElementsByClassName('login');
+const getBtn = document.getElementById('btn');
+const getEmail = document.getElementById('e-mail');
+const getPass = document.getElementById('senha')
+
+/**
+ * Requesito 3: retorna 'Olá, Tryber!' se email e senha certo e 'Email ou senha inválidos.' se inválido;
+ */
+const alertMsg = () => {
+  getBtn.addEventListener('click', () => {
+    if (getEmail.value === 'tryber@teste.com' && getPass.value === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos.');
+    }
+  });
+};
+
+alertMsg();
